b0e49c0874900b8397d8b4006f173d4e6fdc0c89
systementropy <vatsa.saket@gmail.com>
2017-01-29 16:33:41 +0530

    changed user model

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -2,8 +2,12 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var UserSchema   = new Schema({
-    name: String,
-
+    name : String,
+    dob : Date,
+    followersCount : Number,
+    followingCount : Number,
+    followers : [],
+    following : []
 });
 
 module.exports = mongoose.model('User', UserSchema);
