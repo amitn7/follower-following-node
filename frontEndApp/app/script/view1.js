dc4465641bb822deca9044a1eaf843464e5c9db4
systementropy <vatsa.saket@gmail.com>
2017-01-30 15:54:17 +0530

    added get user APi

--- frontEndApp/app/script/view1.js
+++ frontEndApp/app/script/view1.js
@@ -3,14 +3,26 @@
 
     angular.module('myApp.view1', ['ngRoute'])
 
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view1', {
-            templateUrl: '/view/view1.html',
-            controller: 'View1Ctrl'
-        });
-    }])
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.when('/view1', {
+                templateUrl: '/view/view1.html',
+                controller: 'View1Ctrl'
+            });
+        }
+    ])
 
-    .controller('View1Ctrl', [function() {
-
+    .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+        $scope.getUserList = function() {
+            console.log('insise');
+            $http.get('http://localhost:8080/api/user').then(function(data) {
+                console.log(data);
+            });
+        };
+        var initialize = function() {
+            console.log('ninit');
+            $scope.getUserList();
+        };
+        initialize();
     }]);
 }());
